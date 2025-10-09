c27ef4596cd37a2c98288df7f26c68c159757294
abe brege <bregeabe@gmail.com>
2025-10-08 20:18:13 -0400

    another test

--- api/tests/tests.js
+++ api/tests/tests.js
@@ -1,5 +1,5 @@
 import { apply, prepareState } from '../controllers/processor.js'
-import { TWO_QUBIT_BELL_PAIR, H, CNOT } from '../../src/constants.js'
+import { TWO_QUBIT_BELL_PAIR, H, CNOT, S, X } from '../../src/constants.js'
 
 
 function createBellPair() {
@@ -10,8 +10,20 @@ function createBellPair() {
     return res
 }
 
+function testS() {
+    let state = prepareState(1)
+    state = apply(X, state, 0)
+    // console.log(`state after X: ${JSON.stringify(state)}`)
+    state = apply(H, state, 0)
+    // console.log(`state after H: ${JSON.stringify(state)}`)
+    state = apply(S, state, 0)
+    // console.log(`state after S: ${JSON.stringify(state)}`)
+    return state
+}
+
 function run() {
-    console.log("Bell Pair: ", createBellPair());
+    // console.log("Bell Pair: ", createBellPair());
+    testS();
 }
 
 run();
